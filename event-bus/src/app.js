289720072af6e11333f22f823979787239489d04
Arthur Atul <arthur.atul@rubicotech.in>
2022-01-02 17:53:31 +0530

    catch the errors while making axios call

--- event-bus/src/app.js
+++ event-bus/src/app.js
@@ -4,24 +4,38 @@ const axios = require("axios");
 
 const app = express();
 
+const events = [];
+
 
 app.use(express.json());
 
 app.post('/events', async (req, res) => {
 
     const event = req.body;
+    console.log(event.type);
+    events.push(event);
 
-    try {
-        await axios.post('http://localhost:4000/events', event);
-        await axios.post('http://localhost:4001/events', event);
-        await axios.post('http://localhost:4002/events', event);
-        await axios.post('http://localhost:4003/events', event);
-    }catch (error){
-        console.log(error);
-    }
+    await axios.post('http://localhost:4000/events', event).catch(() => {
+
+    });
+    await axios.post('http://localhost:4001/events', event).catch(() => {
+
+    });
+    await axios.post('http://localhost:4002/events', event).catch(() => {
+
+    });
+    await axios.post('http://localhost:4003/events', event).catch(() => {
+
+    });
+
     res.send({status: 'OK'});
 
 });
+
+app.get('/events', (req, res) => {
+    console.log('events', events);
+    res.send(events);
+})
 
 
 module.exports = app;
